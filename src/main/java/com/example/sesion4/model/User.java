cf2b0a5a779cdc0edf12f025ae6fec4f66b8a242
unknown <heavycampos15@gmail.com>
2024-11-28 00:55:58 -0500

    sema4

---
+++ src/main/java/com/example/sesion4/model/User.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sesion4.model;
+
+/**
+ *
+ * @author ANTONY
+ */
+public class User {
+    private String name;
+    private String email;
+    private String apellidoP;
+    private String apellidoM;
+    private String fec_nac;
+    private String direccion;
+    private String edad;
+    private String telef;
+
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getFec_nac() {
+        return fec_nac;
+    }
+
+    public void setFec_nac(String fec_nac) {
+        this.fec_nac = fec_nac;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getTelef() {
+        return telef;
+    }
+
+    public void setTelef(String telef) {
+        this.telef = telef;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
